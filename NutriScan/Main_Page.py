a51c7ba56f56585d39cd7feb1000d20d79a3f296
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2024-10-21 17:13:08 -0500

    Update Main_Page.py

--- NutriScan/Main_Page.py
+++ NutriScan/Main_Page.py
@@ -25,4 +25,4 @@ Use the navigation menu on the left to select a page.
 """)
 
 # Optional: Display a logo or image
-st.image('logo.png', width=200)  # Ensure 'logo.png' is in the same directory or adjust the path accordingly.
+st.image('https://github.com/LuisMorelos/Washu-Hack-Food-Information-Website/blob/main/NutriScan/logo.png', width=200)  # Ensure 'logo.png' is in the same directory or adjust the path accordingly.
