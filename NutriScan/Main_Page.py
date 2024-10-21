8299dd20efc3c9fcef26597a14ef04da05146c34
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2024-10-21 17:06:15 -0500

    Create Main_Page.py

---
+++ NutriScan/Main_Page.py
@@ -0,0 +1,28 @@
+# app.py
+
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(
+    page_title="NutriScan",
+    page_icon="🍽️",p
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Title and description
+st.title("Welcome to the NutriScan 🍽️")
+
+st.write("""
+This app allows you to:
+
+- **Analyze food images** to get nutritional information.
+- **Track your calorie intake.**
+- **Get recipe suggestions.**
+- **Calculate your daily calorie recommendations.**
+- **Calendar**
+Use the navigation menu on the left to select a page.
+""")
+
+# Optional: Display a logo or image
+st.image('logo.png', width=200)  # Ensure 'logo.png' is in the same directory or adjust the path accordingly.
