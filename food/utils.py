4044fe152854f8db0bd1504a96a9f1ba3838de5d
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2024-10-21 17:00:51 -0500

    Delete food directory

--- food/utils.py
+++
@@ -1,8 +0,0 @@
-# utils.py
-
-def format_number(value):
-    if value is not None:
-        formatted = f"{value:.10f}"
-        return f"{formatted.rstrip('0').rstrip('.')}"
-    else:
-        return '0'
